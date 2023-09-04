fafb5dce6c436605ebee5102289aadacec39d995
shenchensucc <shenchensucc@gmail.com>
2023-09-04 16:38:49 -0600

    Update BuddyForeverVoting.js

--- src/BuddyForeverVoting.js
+++ src/BuddyForeverVoting.js
@@ -3,8 +3,9 @@ import React from 'react';
 function BuddyForeverVoting() {
   return (
     <div>
-      <h1>This will be voting list page</h1>
-      <p>This is the voting list page.</p>
+    <h1>@buddy-forever的点赞股东名单</h1>  
+    <h2>This is the voting list for @buddy-forever</h2>
+      <p>@shenchensucc, @htliao, @maiyude, @honoru, @softmetal, @ericet, @wilhb81, @abundancelife, @lovelingling, @wilhb81, @susanli3769, @c-next, @windowglass, @justyy, @liuzg, @dapeng, @cnlonglive, @ety001, @huangzuomin, @lovequeen, @cecilian, @julian2013, @ldsn001, @cheva, @circlebubble, @celeste413</p>
     </div>
   );
 }
