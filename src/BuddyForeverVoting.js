576731071c46cf762ac7801d60526f876616aeb6
shenchensucc <shenchensucc@gmail.com>
2023-09-07 22:49:36 -0600

    Update BuddyForeverVoting.js

--- src/BuddyForeverVoting.js
+++ src/BuddyForeverVoting.js
@@ -5,7 +5,7 @@ function BuddyForeverVoting() {
     <div>
     <h1>@buddy-forever的点赞股东名单</h1>  
     <h2>This is the voting list for @buddy-forever</h2>
-      <p>@shenchensucc, @htliao, @maiyude, @honoru, @softmetal, @ericet, @wilhb81, @abundancelife, @lovelingling, @wilhb81, @susanli3769, @c-next, @windowglass, @justyy, @liuzg, @dapeng, @cnlonglive, @ety001, @huangzuomin, @lovequeen, @cecilian, @julian2013, @ldsn001, @cheva, @circlebubble, @celeste413</p>
+      <p>@shenchensucc, @htliao, @maiyude, @honoru, @softmetal, @ericet, @wilhb81, @abundancelife, @lovelingling, @wilhb81, @susanli3769, @c-next, @windowglass, @justyy, @liuzg, @dapeng, @cnlonglive, @ety001, @huangzuomin, @lovequeen, @cecilian, @julian2013, @ldsn001, @cheva, @circlebubble, @celeste413, @aellly</p>
     </div>
   );
 }
