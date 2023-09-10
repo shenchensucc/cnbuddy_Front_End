faf459c09e370e6491e182a64be3e32521e52b89
shenchensucc <shenchensucc@gmail.com>
2023-09-10 14:08:58 -0600

    Add head

--- src/CnbuddyDelegator.js
+++ src/CnbuddyDelegator.js
@@ -50,6 +50,9 @@ function CnbuddyDelegator() {
   };
 
   return (
+    <head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+    </head>
     <div>
       <h1>cnbuddy代理名单</h1>
       <h2>The List of Delegator</h2>
