146b9525a8d7822b18e3b39d16fcb56c806334e5
shenchensucc <shenchensucc@gmail.com>
2023-09-19 23:04:43 -0600

    Update BuddyForeverVoting.js

--- src/BuddyForeverVoting.js
+++ src/BuddyForeverVoting.js
@@ -6,6 +6,7 @@ function BuddyForeverVoting() {
     <h1>@buddy-forever的点赞股东名单</h1>  
     <h2>This is the voting list for @buddy-forever</h2>
       <p>@shenchensucc, @htliao, @maiyude, @honoru, @softmetal, @ericet, @wilhb81, @abundancelife, @lovelingling, @wilhb81, @susanli3769, @c-next, @windowglass, @justyy, @liuzg, @dapeng, @cnlonglive, @ety001, @huangzuomin, @lovequeen, @cecilian, @julian2013, @ldsn001, @cheva, @circlebubble, @celeste413, @aellly</p>
+      <p>@angelina6688</p>
     </div>
   );
 }
